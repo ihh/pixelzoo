65f656215971a6b4b6b0232e20faa76e311c11e1
Ian Holmes <yam@minter-2.local>
2010-12-13 15:22:42 -0800

    thread outline

--- playstate.h
+++ playstate.h
@@ -11,5 +11,19 @@ typedef struct PlayState {
   char *currentStage, *selectedTool;
 } PlayState;
 
+/*
+  Game threads (all on timers):
+  Evolve thread: evolve board, recalculate overload, sleep
+  Redraw thread: redraw board, sleep
+  Challenge thread: test goals -> award rewards, sleep
+  Tool thread: use current selected tool (if active), recharge inactive tools, sleep
+
+  UI events:
+  Key press (or toolbar touch): select current tool
+  Mouse down (or board touch): set current tool active flag
+  Mouse move (or board touch): set current tool x, y
+  Mouse up (or board touch release): clear tool active flag
+ */
+
 
 #endif /* PLAY_STATE_INCLUDED */
