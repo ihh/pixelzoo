24e309153c62c26f3913866391070bc3ecf7ecd5
Ian Holmes <yam@minter-2.local>
2010-12-13 17:57:11 -0800

    npc

--- npc.h
+++ npc.h
@@ -1,10 +1,13 @@
 #ifndef NPC_INCLUDED
 #define NPC_INCLUDED
 
+#include "stringmap.h"
+
 typedef struct NPC {
+  char *name, *currentStage;
   Dictionary *stageDescription;
-  StringMap *stageChallenges;
-  char *currentStage;
+  StringMap *stageChallenges;  /* map from String's to Challenge's */
+  int invisible;  /* if true, NPC will not appear in lists visible to player */
 } NPC;
 
 #endif /* NPC_INCLUDED */
