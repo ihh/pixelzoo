8de46c4e00b98dfe894867df939d025e244cb525
Ian Holmes <yam@minter-2.local>
2010-12-15 22:39:57 -0800

    muted the colors a bit

--- sdltest.c
+++ sdltest.c
@@ -110,10 +110,10 @@ void init( void )
 
     board = newBoardFromXmlString("<xml><board><size>" QUOTEME(BOARD_SIZE) "</size>"
 "<grammar><particle><name>drifter</name><type>1</type><color><mask>3</mask><hexmul>80</hexmul><hexinc>3f</hexinc></color>"
-"<rule><test><loc><x>1</x></loc><val>0</val><ignore>.05</ignore></test><exec><dest></dest><rshift>32</rshift><fail>.01</fail></exec><exec><src><x>1</x></src><dest></dest><hexinc>49</hexinc><hexmask>0ffb</hexmask></exec><exec><dest><x>1</x></dest><rshift>32</rshift><hexinc>10001</hexinc></exec></rule>"
-"<rule><test><loc><x>-1</x></loc><val>0</val><ignore>.05</ignore></test><exec><dest></dest><rshift>32</rshift></exec><exec><src><x>-1</x></src><dest></dest><hexinc>109</hexinc><hexmask>0ffb</hexmask></exec><exec><dest><x>-1</x></dest><rshift>32</rshift><hexinc>10002</hexinc></exec></rule>"
-"<rule><test><loc><y>1</y></loc><val>0</val><ignore>.05</ignore></test><exec><dest></dest><rshift>32</rshift></exec><exec><src><y>1</y></src><dest></dest><hexinc>409</hexinc><hexmask>0ffb</hexmask></exec><exec><dest><y>1</y></dest><rshift>32</rshift><hexinc>10003</hexinc></exec></rule>"
-"<rule><test><loc><y>-1</y></loc><val>0</val><ignore>.05</ignore></test><exec><dest></dest><rshift>32</rshift></exec><exec><src><y>-1</y></src><dest></dest><hexinc>89</hexinc><hexmask>0ffb</hexmask></exec><exec><dest><y>-1</y></dest><rshift>32</rshift><hexinc>10004</hexinc></exec></rule>"
+"<rule><test><loc><x>1</x></loc><val>0</val><ignore>.05</ignore></test><exec><dest></dest><rshift>32</rshift><fail>.01</fail></exec><exec><src><x>1</x></src><dest></dest><hexinc>49</hexinc><hexmask>0ff9</hexmask></exec><exec><dest><x>1</x></dest><rshift>32</rshift><hexinc>10001</hexinc></exec></rule>"
+"<rule><test><loc><x>-1</x></loc><val>0</val><ignore>.05</ignore></test><exec><dest></dest><rshift>32</rshift></exec><exec><src><x>-1</x></src><dest></dest><hexinc>109</hexinc><hexmask>0ff9</hexmask></exec><exec><dest><x>-1</x></dest><rshift>32</rshift><hexinc>10002</hexinc></exec></rule>"
+"<rule><test><loc><y>1</y></loc><val>0</val><ignore>.05</ignore></test><exec><dest></dest><rshift>32</rshift></exec><exec><src><y>1</y></src><dest></dest><hexinc>409</hexinc><hexmask>0ff9</hexmask></exec><exec><dest><y>1</y></dest><rshift>32</rshift><hexinc>10003</hexinc></exec></rule>"
+"<rule><test><loc><y>-1</y></loc><val>0</val><ignore>.05</ignore></test><exec><dest></dest><rshift>32</rshift></exec><exec><src><y>-1</y></src><dest></dest><hexinc>89</hexinc><hexmask>0ff9</hexmask></exec><exec><dest><y>-1</y></dest><rshift>32</rshift><hexinc>10004</hexinc></exec></rule>"
 "</particle></grammar><init><x>" QUOTEME(HALF_BOARD_SIZE) "</x><y>" QUOTEME(HALF_BOARD_SIZE) "</y><type>1</type></init></board></xml>");
 
     /* palette test */
