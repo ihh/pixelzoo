29de21ce294822f8af9dfab823a83afaa57ceb03
Ian Holmes <yam@smith.local>
2010-12-28 21:01:02 -0800

    xcode

---
+++ xcode/pixelzoo/Classes/pixelzooViewController.h
@@ -0,0 +1,16 @@
+//
+//  pixelzooViewController.h
+//  pixelzoo
+//
+//  Created by Ian Holmes on 12/28/10.
+//  Copyright University of California - Berkeley 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface pixelzooViewController : UIViewController {
+
+}
+
+@end
+
