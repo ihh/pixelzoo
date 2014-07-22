9b769ab83c05db7b029745b60ea4ff94ea773e95
Ian Holmes <github@yam.biowiki.org>
2014-07-22 13:34:42 +1000

    ugugugug

--- scheme/rna.scm
+++ scheme/rna.scm
@@ -2,34 +2,34 @@
 
   ;; RNA
   ;; Vars:
-  ;;  has-fwd-sense-bond (1 bit), fwd-sense-bond-dir (3 bits), has-rev-sense-bond (1 bit), rev-sense-bond-dir (3 bits),
-  ;;  has-fwd-anti-bond (1 bit), fwd-anti-bond-dir (3 bits), has-rev-anti-bond (1 bit), rev-anti-bond-dir (3 bits)
+  ;;  has-fs-bond (1 bit), fs-bond-dir (3 bits), has-rs-bond (1 bit), rs-bond-dir (3 bits),
+  ;;  has-fa-bond (1 bit), fa-bond-dir (3 bits), has-ra-bond (1 bit), ra-bond-dir (3 bits)
   ;;  sense-base (2 bits), anti-base (2 bits), has-anti (1 bit)
   ;; Registers:
-  ;;  (0,1)  location of fwd-sense-bond cell
-  ;;  (2,3)  direction & inverse-direction from origin to fwd-sense-bond cell
-  ;;  (4,5)  direction & inverse-direction from target cell to fwd-sense-bond cell
-  ;;  (6,7)  location of rev-sense-bond cell
-  ;;  (8,9)  direction & inverse-direction from origin to rev-sense-bond cell
-  ;; (10,11) direction & inverse-direction from target cell to rev-sense-bond cell
-  ;; (12,13) location of fwd-anti-bond cell
-  ;; (14,15) direction & inverse-direction from origin to fwd-anti-bond cell
-  ;; (16,17) direction & inverse-direction from target cell to fwd-anti-bond cell
-  ;; (18,19) location of rev-anti-bond cell
-  ;; (20,21) direction & inverse-direction from origin to rev-anti-bond cell
-  ;; (22,23) direction & inverse-direction from target cell to rev-anti-bond cell
+  ;;  (0,1)  location of fs-bond cell
+  ;;  (2,3)  direction & inverse-direction from origin to fs-bond cell
+  ;;  (4,5)  direction & inverse-direction from target cell to fs-bond cell
+  ;;  (6,7)  location of rs-bond cell
+  ;;  (8,9)  direction & inverse-direction from origin to rs-bond cell
+  ;; (10,11) direction & inverse-direction from target cell to rs-bond cell
+  ;; (12,13) location of fa-bond cell
+  ;; (14,15) direction & inverse-direction from origin to fa-bond cell
+  ;; (16,17) direction & inverse-direction from target cell to fa-bond cell
+  ;; (18,19) location of ra-bond cell
+  ;; (20,21) direction & inverse-direction from origin to ra-bond cell
+  ;; (22,23) direction & inverse-direction from target cell to ra-bond cell
   ;; (24,25) location of target cell for move
   ;;  26     complement of sense base
 
-  (define rna-has-fwd-sense-bond-var "has-fwd-sense-bond")
-  (define rna-has-rev-sense-bond-var "has-rev-sense-bond")
-  (define rna-fwd-sense-bond-dir-var "fwd-sense-bond-dir")
-  (define rna-rev-sense-bond-dir-var "rev-sense-bond-dir")
+  (define rna-has-fs-bond-var "has-fs-bond")
+  (define rna-has-rs-bond-var "has-rs-bond")
+  (define rna-fs-bond-dir-var "fs-bond-dir")
+  (define rna-rs-bond-dir-var "rs-bond-dir")
 
-  (define rna-has-fwd-anti-bond-var "has-fwd-anti-bond")
-  (define rna-has-rev-anti-bond-var "has-rev-anti-bond")
-  (define rna-fwd-anti-bond-dir-var "fwd-anti-bond-dir")
-  (define rna-rev-anti-bond-dir-var "rev-anti-bond-dir")
+  (define rna-has-fa-bond-var "has-fa-bond")
+  (define rna-has-ra-bond-var "has-ra-bond")
+  (define rna-fa-bond-dir-var "fa-bond-dir")
+  (define rna-ra-bond-dir-var "ra-bond-dir")
 
   (define rna-sense-base-var "sense-base")
   (define rna-anti-base-var "anti-base")
@@ -41,14 +41,14 @@
     `(particle
       (name ,name)
       (vars
-       (varsize (name ,rna-has-fwd-sense-bond-var) (size 2))
-       (varsize (name ,rna-fwd-sense-bond-dir-var) (size 3))
-       (varsize (name ,rna-has-rev-sense-bond-var) (size 2))
-       (varsize (name ,rna-rev-sense-bond-dir-var) (size 3))
-       (varsize (name ,rna-has-fwd-anti-bond-var) (size 2))
-       (varsize (name ,rna-fwd-anti-bond-dir-var) (size 3))
-       (varsize (name ,rna-has-rev-anti-bond-var) (size 2))
-       (varsize (name ,rna-rev-anti-bond-dir-var) (size 3))
+       (varsize (name ,rna-has-fs-bond-var) (size 2))
+       (varsize (name ,rna-fs-bond-dir-var) (size 3))
+       (varsize (name ,rna-has-rs-bond-var) (size 2))
+       (varsize (name ,rna-rs-bond-dir-var) (size 3))
+       (varsize (name ,rna-has-fa-bond-var) (size 2))
+       (varsize (name ,rna-fa-bond-dir-var) (size 3))
+       (varsize (name ,rna-has-ra-bond-var) (size 2))
+       (varsize (name ,rna-ra-bond-dir-var) (size 3))
        (varsize (name ,rna-sense-base-var) (size 2))
        (varsize (name ,rna-anti-base-var) (size 2))
        (varsize (name ,rna-has-anti-var) (size 1)))
@@ -77,61 +77,73 @@
   ;; ...attempt detach, moving into target cell if it is empty.
 
 
-  ;; helper function for iterating over all possible combinations of bonds (a "cascade")
+  ;; helper functions for iterating over all possible combinations of bonds (a "cascade")
   ;; argument list of cascade-func is as follows:
   ;;  (cascade-func tag has-bond-var bond-dir-var partner-has-bond-var partner-bond-dir-var bond-base-reg next-func)
   ;; returns a function that takes arg list similar to init-args
   ;; final-func also takes arg list similar to init-args
-  (define (do-cascade cascade-func final-func init-args)
+  (define (ds-cascade cascade-func final-func init-args)
     (apply
      (cascade-func
       "-ra"
-      rna-has-rev-anti-bond-var
-      rna-rev-anti-bond-dir-var
-      rna-has-fwd-sense-bond-var
-      rna-fwd-sense-bond-dir-var
-      rna-has-fwd-anti-bond-var
-      rna-fwd-anti-bond-dir-var
+      rna-has-ra-bond-var
+      rna-ra-bond-dir-var
+      rna-has-fs-bond-var
+      rna-fs-bond-dir-var
+      rna-has-fa-bond-var
+      rna-fa-bond-dir-var
+      "!ra-bond-already-antisense"  ;; error
       2
       18
       (cascade-func
        "-fa"
-       rna-has-fwd-anti-bond-var
-       rna-fwd-anti-bond-dir-var
-       rna-has-rev-sense-bond-var
-       rna-rev-sense-bond-dir-var
-       rna-has-rev-anti-bond-var
-       rna-rev-anti-bond-dir-var
+       rna-has-fa-bond-var
+       rna-fa-bond-dir-var
+       rna-has-rs-bond-var
+       rna-rs-bond-dir-var
+       rna-has-ra-bond-var
+       rna-ra-bond-dir-var
+       "!fa-bond-already-antisense"  ;; error
        1
        12
-       (cascade-func
-	"-rs"
-	rna-has-rev-sense-bond-var
-	rna-rev-sense-bond-dir-var
-	rna-has-fwd-sense-bond-var
-	rna-fwd-sense-bond-dir-var
-	rna-has-fwd-anti-bond-var
-	rna-fwd-anti-bond-dir-var
-	2
-	6
-	(cascade-func
-	 "-fs"
-	 rna-has-fwd-sense-bond-var
-	 rna-fwd-sense-bond-dir-var
-	 rna-has-rev-sense-bond-var
-	 rna-rev-sense-bond-dir-var
-	 rna-has-rev-anti-bond-var
-	 rna-rev-anti-bond-dir-var
-	 1
-	 0
-	 final-func))))
+       (ss-cascade-func cascade-func final-func)))
      init-args))
+
+  (define (ss-cascade cascade-func final-func init-args)
+    (apply
+     (ss-cascade-func cascade-func final-func)
+     init-args))
+
+  (define (ss-cascade-func cascade-func final-func)
+    (cascade-func
+     "-rs"
+     rna-has-rs-bond-var
+     rna-rs-bond-dir-var
+     rna-has-fs-bond-var
+     rna-fs-bond-dir-var
+     rna-has-fa-bond-var
+     rna-fa-bond-dir-var
+     rna-ra-bond-dir-var
+     2
+     6
+     (cascade-func
+      "-fs"
+      rna-has-fs-bond-var
+      rna-fs-bond-dir-var
+      rna-has-rs-bond-var
+      rna-rs-bond-dir-var
+      rna-has-ra-bond-var
+      rna-ra-bond-dir-var
+      rna-fa-bond-dir-var
+      1
+      0
+      final-func))))
 
   ;; create the cascade for the top-level switch that tests existence & integrity of bonds
   (define (rna-move-subrule)
     (subrule
      "rna-move-subrule"
-     (do-cascade rna-bond-cascade rna-drift-rule `("try-random-step-with-anti" "" () ,moore-dirs))))
+     (ds-cascade rna-bond-cascade rna-drift-rule `("try-random-step-with-anti" "" () ,moore-dirs))))
 
   ;; rna-bond-cascade: the function for creating the bond verification cascade
   ;; if has-bond-var is TRUE, verify that the bond is mutual, and add to confirmed-bond-list
@@ -141,36 +153,41 @@
 	   has-bond-var bond-dir-var
 	   sense-partner-has-bond-var sense-partner-bond-dir-var
 	   anti-partner-has-bond-var anti-partner-bond-dir-var
+	   merged-bond-dir-var
 	   expected-partner-has bond-base-reg next-in-cascade)
     (lambda (subrule-prefix subrule-suffix confirmed-bond-list candidate-nbr-dirs)
       (switch-var
-       origin self-type bond-var
+       origin self-type has-bond-var
        `((0 ,(next-in-cascade subrule-prefix subrule-suffix confirmed-bond-list candidate-nbr-dirs))
-	 (1 ,(bind-and-verify tag bond-dir-var
+	 (1 ,(bind-and-verify tag 0 bond-dir-var
 			      sense-partner-has-bond-var sense-partner-bond-dir-var
 			      expected-partner-has bond-base-reg next-in-cascade
 			      subrule-prefix subrule-suffix confirmed-bond-list candidate-nbr-dirs))
-	 (2 ,(bind-and-verify tag bond-dir-var
+	 (2 ,(bind-and-verify tag 1 bond-dir-var
 			      anti-partner-has-bond-var anti-partner-bond-dir-var
 			      expected-partner-has bond-base-reg next-in-cascade
 			      subrule-prefix subrule-suffix confirmed-bond-list candidate-nbr-dirs))))))
 
-  (define (bind-and-verify tag bond-dir-var partner-has-bond-var partner-bond-dir-var
+  (define (make-connect-tag goes-to-anti)
+    (if (goes-to-anti) "2a" "2s"))
+
+  (define (bind-and-verify tag goes-to-anti bond-dir-var partner-has-bond-var partner-bond-dir-var
 			   expected-partner-has-bond-var bond-base-reg next-in-cascade
 			   subrule-prefix subrule-suffix confirmed-bond-list candidate-nbr-dirs)
-    (bind-moore-dir
-     bond-dir-var
-     (lambda (loc dir)
-       (let* ((inv-dir (moore-back dir)))
-	 ;; bond verification goes here
-	 (next-in-cascade
-	  subrule-prefix
-	  (string-append subrule-suffix tag)
-	  (cons (list bond-dir-var loc dir inv-dir bond-base-reg partner-has-bond-var partner-bond-dir-var)
-		confirmed-bond-list)
-	  (grep
-	   (lambda (nbr-dir)
-	     (moore-neighbor? (loc-minus loc (moore-loc nbr-dir)))) candidate-nbr-dirs))))))
+    (let ((goes-to-anti-tag (make-connect-tag goes-to-anti)))
+      (bind-moore-dir
+       bond-dir-var
+       (lambda (loc dir)
+	 (let* ((inv-dir (moore-back dir)))
+	   ;; bond verification goes here
+	   (next-in-cascade
+	    subrule-prefix
+	    (string-append subrule-suffix tag goes-to-anti-tag)
+	    (cons (list bond-dir-var loc dir inv-dir bond-base-reg partner-has-bond-var partner-bond-dir-var)
+		  confirmed-bond-list)
+	    (grep
+	     (lambda (nbr-dir)
+	       (moore-neighbor? (loc-minus loc (moore-loc nbr-dir)))) candidate-nbr-dirs)))))))
 
 
   ;; rna-drift-rule(confirmed-bond-list,candidate-nbr-dirs): the rule at the bottom of the bond verification cascade
@@ -215,26 +232,60 @@
 
   ;; random-step-subrule-cascade: the function for creating the various drift rules
   (define (random-step-subrule-cascade
-	   tag has-bond-var bond-dir-var
-	   partner-has-bond-var partner-bond-dir-var
+	   tag
+	   has-bond-var bond-dir-var
+	   sense-partner-has-bond-var sense-partner-bond-dir-var
+	   anti-partner-has-bond-var anti-partner-bond-dir-var
+	   merged-bond-dir-var
 	   expected-partner-has-bond-var bond-base-reg next-in-cascade)
-    (lambda (subrule-prefix subrule-suffix self-has-anti confirmed-bond-reg-list)
-      (begin
-	(next-in-cascade
-	 subrule-prefix
-	 (string-append subrule-suffix tag)
-	 self-has-anti
-	 (cons (list has-bond-var bond-dir-var partner-has-bond-var partner-bond-dir-var bond-base-reg)
-	       confirmed-bond-reg-list))
-	(next-in-cascade subrule-prefix subrule-suffix self-has-anti confirmed-bond-reg-list))))
+    (let ((goes-to-anti-tag (make-connect-tag 1))
+	  (goes-to-sense-tag (make-connect-tag 0)))
+      (lambda (subrule-prefix subrule-suffix self-has-anti confirmed-bond-reg-list)
+	(begin
+	  (next-in-cascade
+	   subrule-prefix
+	   (string-append subrule-suffix tag goes-to-sense-tag)
+	   self-has-anti
+	   (cons (list has-bond-var bond-dir-var sense-partner-has-bond-var sense-partner-bond-dir-var merged-bond-dir-var bond-base-reg)
+		 confirmed-bond-reg-list))
+	  (next-in-cascade
+	   subrule-prefix
+	   (string-append subrule-suffix tag goes-to-anti-tag)
+	   self-has-anti
+	   (cons (list has-bond-var bond-dir-var anti-partner-has-bond-var anti-partner-bond-dir-var merged-bond-dir-var bond-base-reg)
+		 confirmed-bond-reg-list))
+	  (next-in-cascade subrule-prefix subrule-suffix self-has-anti confirmed-bond-reg-list)))))
 
   ;; cascade to define drift rules & associated subrules
-  (do-cascade random-step-subrule-cascade make-random-step-rule `("try-random-step-ds" "" 1 ()))  ;; single-stranded
-  (do-cascade random-step-subrule-cascade make-random-step-rule `("try-random-step-ss" "" 0 ()))  ;; double-stranded
+  (ds-cascade random-step-subrule-cascade make-random-step-rule `("try-random-step-ds" "" 1 ()))  ;; double-stranded
+  (ss-cascade random-step-subrule-cascade make-random-step-rule `("try-random-step-ss" "" 0 ()))  ;; single-stranded
 
   ;; the function at the bottom of the drift rule cascade
+  (define rna-merge-subrule-name "rna-merge-subrule")
+  (define (merge-bonds confirmed-bond-reg-list)
+    (if
+     (null? confirmed-bond-reg-list)
+     nop-rule
+     (let* ((confirmed-bond-reg (car confirmed-bond-reg-list))
+	    (has-bond-var (car confirmed-bond-reg))
+	    (bond-dir-var (cadr confirmed-bond-reg))
+	    (partner-has-bond-var (caddr confirmed-bond-reg))
+	    (partner-bond-dir-var (cadddr confirmed-bond-reg))
+	    (merged-bond-dir-var (caddddr confirmed-bond-reg))
+	    (bond-base-reg (cadddddr confirmed-bond-reg))
+	    (rest-of-confirmed-bond-reg-list (cdr confirmed-bond-reg-list))
+	    (merge-remaining-bonds (merge-bonds rest-of-confirmed-bond-reg-list)))
+       (indirect-set-var-from-register
+	'(24 25)
+	self-type merged-bond-dir-var (+ bond-base-reg 4)
+	(indirect-set-var-from-register
+	 (list bond-base-reg (+ bond-base-reg 1))
+	 self-type partner-bond-dir-var (+ bond-base-reg 5))
+	merge-remaining-bonds))))
+
   (define (make-random-step-rule subrule-prefix subrule-suffix self-has-anti confirmed-bond-reg-list)
-    (let* ((subrule-name (string-append subrule-prefix subrule-suffix)))
+    (let* ((subrule-name (string-append subrule-prefix subrule-suffix))
+	   (qualified-rna-merge-subrule-name (string-append rna-merge-subrule-name subrule-suffix)))
       (begin
 	(subrule
 	 subrule-name
@@ -251,36 +302,21 @@
 		`(0   ;; (no antisense base in source or target cell)
 		  ,(indirect-compare-var-to-register
 		    '(24 25) self-type rna-sense-base 26
-		    `(eq "rna-merge-subrule")  ;; complementary: merge
+		    `(eq ,qualified-rna-merge-subrule-name)  ;; complementary: merge
 		    `(neq  ;; non-complementary: merge with probability rna-merge-mismatch-prob
 		      ,(random-rule
 			rna-merge-mismatch-prob
-			"rna-merge-subrule")))))))))))))
-
-  ;; merge subrule
-  (subrule
-   "rna-merge-subrule"
-   (copy-self-var-to-indirect
-    self-type rna-sense-base-var '(24 25) self-type rna-anti-base-var
-    (copy-self-var-to-indirect
-     self-type rna-has-fwd-sense-bond-var '(24 25) self-type rna-has-fwd-anti-bond-var
-     (copy-self-var-to-indirect
-      self-type rna-has-rev-sense-bond-var '(24 25) self-type rna-has-rev-anti-bond-var
-      (switch-var
-       origin self-type rna-has-fwd-sense-bond-var
-       `((0 (goto "rna-merge-subrule-test-rev"))
-	 (1
-	  ,(indirect-set-var-from-register
-	    '(24 25) self-type rna-fwd-anti-bond-dir-var 4
-	    (indirect-set-var-from-register
-	     '(0 1) self-type
-;; uh oh. does our fwd-sense bond go to neighbor's rev-sense or neighbor's rev-anti?
-;; do we need more vars to track this?
-;; that will mean combinatorially more cases to test...
-;; i think we in fact need to expand has-fwd-sense, has-rev-sense, has-fwd-anti & has-rev-anti to 2-bit vars
-;;  0 for no bond, 1 for bond-to-sense, 2 for bond-to-anti
-;; need to update do-cascade to pass in vars for both possibilities
-;; single stranded always uses the sense bonds
+			qualified-rna-merge-subrule-name))))))))))
+	;; merge subrule
+	(subrule
+	 qualified-rna-merge-subrule-name
+	 (copy-self-var-to-indirect
+	  self-type rna-sense-base-var '(24 25) self-type rna-anti-base-var
+	  (copy-self-var-to-indirect
+	   self-type rna-has-fs-bond-var '(24 25) self-type rna-has-fa-bond-var
+	   (copy-self-var-to-indirect
+	    self-type rna-has-rs-bond-var '(24 25) self-type rna-has-ra-bond-var
+	    (merge-bonds confirmed-bond-reg-list))))))))
 
   ;; function to update bond vars in a drift move
   (define (update-registers confirmed-bond-reg-list next-rule)
